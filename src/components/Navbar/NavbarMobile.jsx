dac4c00072ad169c687a1743c04cb36123b41e4a
falguni-mondal <career.falguni@gmail.com>
2025-08-24 00:32:39 +0530

    Mobile Navbar design Adjustments

--- src/components/Navbar/NavbarMobile.jsx
+++ src/components/Navbar/NavbarMobile.jsx
@@ -24,32 +24,32 @@ const NavbarMobile = ({navReveal, setNavReveal}) => {
         },
       })
       gsap.to(mobileNavRef.current, {
-        width: "43vw",
+        width: "42vw",
         duration: 1,
         scrollTrigger: {
           trigger: mobileNavRef.current,
           start: "top 20%",
-          end: "top 20%",
+          end: "top 0%",
           scrub: 1,
         },
       })
     }
   }, [])
   return (
-    <div ref={mobileNavRef} className='mobile-navbar-container lg:hidden fixed bottom-5 left-1/2 transform -translate-x-1/2 w-[40vw] p-1 rounded-full bg-[#5f5f5f38] backdrop-blur-[20px] flex justify-between items-center z-[999]'>
-      <div className="mobile-logo h-[3rem] w-[3rem] shrink-0">
+    <div ref={mobileNavRef} className='mobile-navbar-container lg:hidden fixed bottom-5 left-1/2 transform -translate-x-1/2 w-[38vw] p-1 rounded-full bg-[#5f5f5f38] backdrop-blur-[20px] flex justify-between items-center z-[999]'>
+      <div className="mobile-logo h-[3rem] w-[3rem]">
         <img className='w-full h-full object-cover' src="icon.png" alt="" />
       </div>
-      <a href='#hero-section' ref={topLinkRef} className="top-btn h-[3rem] w-[3rem] rounded-full bg-black flex items-center justify-center shrink-0">
+      <a href='#hero-section' ref={topLinkRef} className="top-btn h-[3rem] w-[3rem] rounded-full bg-black flex items-center justify-center">
         <IoMdArrowUp className='text-[1.3rem] text-zinc-200' />
       </a>
       {
         navReveal ?
-          <span onClick={() => setNavReveal(false)} className="hamburger-icon h-[3rem] w-[3rem] rounded-full bg-zinc-100 flex items-center justify-center shrink-0" id='nav-icon'>
+          <span onClick={() => setNavReveal(false)} className="hamburger-icon h-[3rem] w-[3rem] rounded-full bg-zinc-100 flex items-center justify-center" id='nav-icon'>
             <GrClose className='text-[1.3rem]' />
           </span>
           :
-          <span onClick={() => setNavReveal(true)} className="hamburger-icon h-[3rem] w-[3rem] rounded-full bg-zinc-100 flex items-center justify-center shrink-0" id='nav-icon'>
+          <span onClick={() => setNavReveal(true)} className="hamburger-icon h-[3rem] w-[3rem] rounded-full bg-zinc-100 flex items-center justify-center" id='nav-icon'>
             <GrAppsRounded className='text-[1.3rem]' />
           </span>
       }
